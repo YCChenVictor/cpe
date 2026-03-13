01c123b94cdc48e5146225230df47f6d0eafb880
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 01:05:55 +0800

    study 100

--- questions/100/main.py
+++ questions/100/main.py
@@ -1,30 +1,20 @@
-import sys
-
-data = sys.stdin.buffer.read().split()
-out = []
-it = iter(data)
-
-def calculation(n):
-    if n == 1:
-        return 1
-
-    if n % 2:
-        return 1 + calculation(3*n+1)
-    else:
-        return 1 + calculation(n//2)
-
-for a in it:
-    b = next(it, None)
-    if b is None:
+while True:
+    try:
+        a, b = map(int, input().split())
+        start = min(a, b)
+        end = max(a, b)
+        answer = 0
+        for i in range(start, end + 1):
+            n = i
+            count = 1
+            while (n != 1):
+                if (n % 2 == 1):
+                    n = 3 * n + 1
+                else:
+                    n = n / 2
+                count += 1
+            if count > answer:
+                answer = count
+        print(a, b, answer)
+    except EOFError:
         break
-    i = int(a); j = int(b)
-    l, h = (i, j) if i < j else (j, i)
-    
-    best = 0
-    for x in range(l, h+1):
-        best = max(best, calculation(x))
-
-    out.append(f"{i} {j} {best}")
-    
-
-sys.stdout.write("\n".join(out))
