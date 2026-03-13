f61d1c4e37f0268df82a1d66d679baca6d43e393
Victor Chen <victor.yccchen1@gmail.com>
2026-03-13 16:20:27 +0800

    study 10055

--- questions/10055/main.py
+++ questions/10055/main.py
@@ -1,7 +1,6 @@
-import sys
-
-data = sys.stdin.read().splitlines()
-
-for line in data:
-    numbers = line.split()
-    print(abs(int(numbers[0]) - int(numbers[1])))
+while True:
+    try:
+        a, b = map(int, input().split())
+        print(abs(a - b))
+    except EOFError:
+        break
