0f81c02cc56f2fbcf6d22930963fa4e160ec3725
Victor Chen <victor.yccchen1@gmail.com>
2026-03-13 10:04:24 +0800

    study 10041

--- questions/10041/main.py
+++ questions/10041/main.py
@@ -1,16 +1,18 @@
 import sys
 
-data = sys.stdin.read().splitlines()
+# Ok, then input GPT to gradually correct the answers
 
-for i in range(1, len(data)):
-    line = data[i]
-    numbers = line.split()
-    num_of_relative = int(numbers[0])
-    streets = list(map(int, numbers[1:]))
-    streets.sort()
-    medium = streets[num_of_relative // 2]
-    answer = 0
-    for j in range(0, len(streets)):
-        answer += abs(streets[j] - medium)
-        
-    print(answer)
+lines = sys.stdin.read().splitlines()
+
+for i in range(1, len(lines)):
+    current_line = lines[i]
+    raw_numbers = current_line.split()
+    num_of_numbers = int(raw_numbers[0])
+    numbers = [int(raw_numbers[j]) for j in range(1, num_of_numbers + 1)]
+    numbers.sort()
+
+    sum = 0
+    medium = numbers[num_of_numbers // 2]
+    for num in numbers:
+        sum += abs(medium - num)
+    print(sum)
