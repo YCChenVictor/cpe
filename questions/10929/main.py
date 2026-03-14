8a4c1c57581c3dd1d0ad212f4e5629e12f5676db
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 10:18:42 +0800

    study 10929

--- questions/10929/main.py
+++ questions/10929/main.py
@@ -1,12 +1,11 @@
-import sys
-
-lines = sys.stdin.read().splitlines()
-
-for line in lines:
-    if line == '0':
+while True:
+    try:
+        number = int(input())
+        if number == 0:
+            continue
+        if number % 11 == 0:
+            print(f"{number} is a multiple of 11.")
+        else:
+            print(f"{number} is not a multiple of 11.")
+    except EOFError:
         break
-
-    if int(line) % 11 == 0:
-        print(f"{line} is a multiple of 11.")
-    else:
-        print(f"{line} is not a multiple of 11.")
