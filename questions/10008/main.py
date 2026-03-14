3278821fd7646d025071af34559ff14a4dc04641
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 11:43:07 +0800

    study 10008

--- questions/10008/main.py
+++ questions/10008/main.py
@@ -1,33 +1,17 @@
-import sys
+t = int(input())
 
-data = sys.stdin.read().strip()
+collection = {}
+for _ in range(t):
+    line = input()
+    for text in line:
+        if not (("a" <= text <= "z") or ("A" <= text <= "Z")):
+            continue
+        cap_letter = text.capitalize()
+        if collection.get(cap_letter) is None:
+            collection[cap_letter] = 0
+        collection[cap_letter] = collection[cap_letter] + 1
 
-collect = {}
-for i in range(0, len(data)):
-    current_letter = data[i].upper()
-    if not ("A" <= current_letter <= "Z"):
-        continue
-
-    count = collect.get(current_letter)
-
-    if count is None:
-        collect[current_letter] = 1
-    else:
-        collect[current_letter] += 1
-
-collect2 = {}
-numbers = set()
-for k, v in collect.items():
-    exist = collect2.get(v)
-    if exist is None:
-        collect2[v] = []
-    collect2[v].append(k)
-    numbers.add(v)
-numbers = sorted(numbers)
-numbers.reverse()
-
-for i in range(0, len(numbers)):
-    letters = collect2[numbers[i]]
-    letters.sort()
-    for k in range(0, len(letters)):
-        print(f"{letters[k]} {numbers[i]}")
+items = list(collection.items())
+items.sort(key=lambda item: (-item[1], item[0]))
+for item in items:
+    print(f"{item[0]} {item[1]}")
