e9341e2e9984852a76c70b42bf4532b3560a1f94
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 23:10:41 +0800

    study 948

--- questions/948/main.py
+++ questions/948/main.py
@@ -1,35 +1,28 @@
-import sys
+fib = [1, 2]
 
-data = sys.stdin.read().split()
-
-num_of_nums = data[0]
-
-threshold = 100000000
-fib_numbers = []
-
-fib_0 = 0
-fib_1 = 1
-current_value = fib_0 + fib_1
-fib_numbers = []
+i = 1
+new = 0
 while True:
-    current_value = fib_0 + fib_1
-
-    if current_value > threshold:
+    new = fib[i - 1] + fib[i]
+    if new > 100000000:
         break
-    fib_numbers.append(current_value)
-    fib_0 = fib_1
-    fib_1 = current_value
+    fib.append(new)
+    i += 1
 
-fib_numbers.reverse()
+fib.reverse()
 
-for i in range(1, len(data)):
-    left = int(data[i])
-    answer = ''
-    for j in range(0, len(fib_numbers)):
-        if fib_numbers[j] <= left:
-            left -= fib_numbers[j]
-            answer += '1'
-        else:
-            answer += '0'
-
-    print(f"{int(data[i])} = {answer.lstrip('0')} (fib)")
+cases = input()
+while True:
+    try:
+        answer = ""
+        num = int(input())
+        to_extract = num
+        for extract in fib:
+            if to_extract >= extract:
+                to_extract -= extract
+                answer += "1"
+            else:
+                answer += "0"
+        print(f"{num} = {int(answer)} (fib)")
+    except EOFError:
+        break
