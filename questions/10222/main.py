ab9593948c104613fa9f86f8bb0a15b761a53d56
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 11:57:26 +0800

    study 10222

--- questions/10222/main.py
+++ questions/10222/main.py
@@ -1,14 +1,14 @@
-import sys
+mapping = "1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./"
 
-lines = sys.stdin.read().splitlines()
-
-keyboard = "qwertyuiop[]asdfghjkl;'zxcvbnm,."
-mapping = {" ": " "}
-for i in range(2, len(keyboard)):
-    mapping[keyboard[i]] = keyboard[i-2]
-
-for line in lines:
-    answer = ''
-    for letter in line:
-        answer += mapping[letter]
-    print(answer)
+while True:
+    try:
+        line = input()
+        answer = ""
+        for letter in line:
+            if letter == " ":
+                answer += " "
+            else:
+                answer += mapping[mapping.index(letter) - 2]
+        print(answer)
+    except EOFError:
+        break
