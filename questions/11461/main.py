f9b76ce5e87ca95ad5bc82b4e20c76b3f5ae8fe0
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 20:24:10 +0800

    study 11461

--- questions/11461/main.py
+++ questions/11461/main.py
@@ -1,21 +1,13 @@
-import sys
 import math
 
-lines = sys.stdin.read().splitlines()
-
-def is_square(n):
-    if n == 0:
-         return False
-
-    return n == math.isqrt(n) ** 2
-
-for line in lines:
-    [start, end] = line.split()
-    start = int(start)
-    end = int(end)
-
-    answer = math.isqrt(end) - math.isqrt(start)
-    if is_square(start):
-        answer += 1
-
-    print(answer)
+while True:
+    try:
+        line = input()
+        if line == "0 0":
+            break
+        start, end = map(int, line.split())
+        s_root = start ** 0.5
+        e_root = end ** 0.5
+        print(math.floor(e_root) - math.ceil(s_root) + 1)
+    except EOFError:
+        break
