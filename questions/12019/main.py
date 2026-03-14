f0af94dcb5ad10d4695b38c9adef6e97f61f0dd1
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 14:36:35 +0800

    study 12019

--- questions/12019/main.py
+++ questions/12019/main.py
@@ -1,32 +1,10 @@
-import sys
+t = int(input())
 
-# 2011 rules:
-# 1/10, 2/21, 4/4, 5/9, 6/6, 7/11, 8/8, 9/5, 10/10, 11/7, 12/12 are all Mondays
+bases = [10, 21, 7, 4, 9, 6, 11, 8, 5, 10, 7, 12]
+mapping = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-rules = {
-  '1': '10',
-  '2': '21',
-  '3': '14',
-  '4': '4',
-  '5': '9',
-  '6': '6',
-  '7': '11',
-  '8': '8',
-  '9': '5',
-  '10': '10',
-  '11': '7',
-  '12': '12'
-}
+for _ in range(t):
+    month, day = map(int, input().split())
+    difference = day - bases[month - 1] # day more than base
 
-weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-lines = sys.stdin.read().splitlines()
-
-num_of_cases = lines[0]
-for i in range(1, len(lines)):
-    [month, day] = lines[i].split()
-    base = int(rules[month])
-    month = int(month)
-    day = int(day)
-    difference = (day - base) % 7
-    print(weekday[difference])
+    print(mapping[difference % 7])
