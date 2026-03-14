be75fa237677deb6d496bedb8ea873ad744e495c
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 22:47:15 +0800

    study 10093

--- questions/10093/main.py
+++ questions/10093/main.py
@@ -1,39 +1,31 @@
-import sys
-import string
+base_mapping = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-lines = sys.stdin.read().splitlines()
-
-base = string.digits + string.ascii_uppercase + string.ascii_lowercase
-chars = list(base)
-
-dict = {}
-for i in range(0, len(chars)):
-    dict[chars[i]] = i
-
-for line in lines:
-    elements = list(line)
-    largest_number_index = 0
-    clean_number = ''
-    element_sum = 0
-    for element in elements:
-        if not dict.get(element):
+while True:
+    try:
+        num = input()
+        base = 2
+        if num == "0":
+            print(base)
             continue
-        if dict[element] > largest_number_index:
-            largest_number_index = dict[element]
-        clean_number += element
-        element_sum += dict[element]
-    first_base_guess = largest_number_index + 1
-    
-    base_guess = first_base_guess
-    while base_guess <= 62:
-        if element_sum == 0:
-            base_guess = 2
-            break
-        if element_sum % (base_guess - 1) == 0:
-            break
-        base_guess += 1
-    
-    if base_guess > 62:
-        print("such number is impossible!")
-    else:
-        print(base_guess) 
+        # first guess, must > any digits
+        sum = 0
+        for digit in num:
+            if digit in ["+", "-", " "]:
+                continue
+            if base_mapping.index(digit) >= base:
+                base = base_mapping.index(digit) + 1
+            sum += base_mapping.index(digit)
+
+        answer = -1  # this is really important
+        for i in range(base, 62 + 1):  # check the division
+            if sum % (i - 1) == 0:
+                answer = i
+                break  # smallest
+
+        if answer == -1:
+            print("such number is impossible!")
+        else:
+            print(answer)
+        
+    except EOFError:
+        break
