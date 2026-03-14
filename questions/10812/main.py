2fc4d097d3a8ffab4f46ba382f23bfb1ad0b55b0
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 19:41:43 +0800

    study 10812

--- questions/10812/main.py
+++ questions/10812/main.py
@@ -1,17 +1,15 @@
-import sys
+t = int(input())
 
-lines = sys.stdin.read().splitlines()
-
-num_of_cases = int(lines[0])
-for i in range(1, num_of_cases + 1):
-    [a, b] = lines[i].split()
-    a = int(a)
-    b = int(b)
-    x = (a + b) / 2
-    y = (a - b) / 2
-    
-
-    if (y < 0 or x % 1 != 0 or y % 1 != 0):
+for _ in range(t):
+    # x + y = sum
+    # x - y = diff
+    # x > y
+    # x = (sum + diff) / 2
+    # y = (sum - diff) / 2
+    sum, diff = map(int, input().split())
+    x = (sum + diff) / 2
+    y = (sum - diff) / 2
+    if (not x.is_integer()) or (not y.is_integer()) or (x < 0) or (y < 0):
         print("impossible")
     else:
         print(int(x), int(y))
