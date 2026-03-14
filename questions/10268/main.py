406f791db847fa76873cc43eb8ced6ed72c1cb32
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 16:11:09 +0800

    study 10268

--- questions/10268/main.py
+++ questions/10268/main.py
@@ -1,12 +1,19 @@
-import sys
-
-lines = sys.stdin.read().splitlines()
-
-for i in range(0, len(lines), 2):
-    input = int(lines[i])
-    degrees = lines[i+1].split()
-    n = len(degrees) - 1
-    derivates = [(n - x) * int(degrees[x]) for x in range(0, len(degrees) - 1)]
-    values = [derivates[x] * input ** (len(derivates) - x - 1) for x in range(0, len(derivates))]
-    print(sum(values))
-
+while True:
+    try:
+        x = int(input())
+        params = list(map(int, input().split()))
+        length = len(params)
+        dev_params = []
+        for i in range(length):
+            dev_params.append((length - i - 1) * params[i])
+        if x == 0:
+            print(int(dev_params[len(dev_params) - 2]))
+            continue
+        answer = 0
+        power = 0
+        for i in range(length - 1, -1, -1):
+            answer += x ** (power - 1) * dev_params[i]
+            power += 1
+        print(int(answer))
+    except EOFError:
+        break
