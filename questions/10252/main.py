0bf28a5bccd0fab81a11515875f9378ba78fd95f
Victor Chen <victor.yccchen1@gmail.com>
2026-03-14 12:43:59 +0800

    study 10252

--- questions/10252/main.py
+++ questions/10252/main.py
@@ -1,32 +1,21 @@
-import sys
+while True:
+    try:
+        first = input()
+        second = input()
+        
+        count1 = [0] * 26
+        count2 = [0] * 26
 
-lines = sys.stdin.read().splitlines()
+        for letter in first:
+            count1[ord(letter) - ord('a')] += 1
+        for letter in second:
+            count2[ord(letter) - ord('a')] += 1
 
-for i in range(0, len(lines), 2):
-    first = list(lines[i])
-    second = list(lines[i+1])
+        ans = ""
+        for i in range(26):
+            common = min(count1[i], count2[i])
+            ans += chr(i + ord('a')) * common
 
-    first_dict = {}
-    for letter in first:
-        if first_dict.get(letter) == None:
-            first_dict[letter] = 0
-        first_dict[letter] += 1
-
-    second_dict = {}
-    for letter in second:
-        if second_dict.get(letter) == None:
-            second_dict[letter] = 0
-        second_dict[letter] += 1
-
-    both_keys = first_dict.keys() & second_dict.keys()
-
-    answer = ""
-    for key in both_keys:
-        first_amount = first_dict[key]
-        second_amount = second_dict[key]
-        min_amount = min(first_amount, second_amount)
-
-        answer += key * min_amount
-
-
-    print("".join(sorted(answer)))
+        print(ans)
+    except EOFError:
+        break
