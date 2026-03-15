760df941aa7ea37627e376ede34aac746ba6955f
Victor Chen <victor.yccchen1@gmail.com>
2026-03-15 15:16:14 +0800

    study 10190

--- questions/10190/main.py
+++ questions/10190/main.py
@@ -1,27 +1,18 @@
-import sys
-
-lines = sys.stdin.read().splitlines()
-
-for line in lines:
-    [n, m] = line.split()
-    n = int(n)
-    m = int(m)
-    
-    if n <= 1  or m == 0 or n < m:
-        print(f"Boring!")
-        continue
-    
-    answer = [n]
-    while True:
-        if n % m == 0:
-            answer.append(int(n/m))
-        n = n/m
-        if n == 1 or n/m >= n:
-            break
-    
-    if len(answer) == 0:
-        print(f"Boring!")
-    elif answer[len(answer) - 1] == 1:
-        print(f"{' '.join(map(str, answer))}")
-    else:
-        print(f"Boring!")
+while True:
+    try:
+        den, nom = map(int, input().split())
+        if (den == 0) or (nom == 0) or (den < nom) or (nom == 1):
+            print("Boring!")
+            continue
+        answer = []
+        while den > 0:
+            answer.append(str(den))
+            if den % nom != 0:
+                break
+            den //= nom
+        if answer[-1] == '1':
+            print(" ".join(answer))
+        else:
+            print("Boring!")
+    except EOFError:
+        break
