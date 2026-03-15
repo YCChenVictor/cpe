31f9d7812feecf9ec2159ba679ab3be1e64cd9ea
Victor Chen <victor.yccchen1@gmail.com>
2026-03-15 16:07:16 +0800

    study 11417

--- questions/11417/main.py
+++ questions/11417/main.py
@@ -1,12 +1,12 @@
-import sys
 import math
 
-lines = sys.stdin.read().splitlines()
+while True:
+    number = int(input())
+    if number == 0:
+        break
 
-for line in lines:
     G = 0
-    for i in range(1, int(line)):
-        for j in range(i+1, int(line)+1):
+    for i in range(1, number):
+        for j in range(i+1, number + 1):
             G += math.gcd(i, j)
-
     print(G)
