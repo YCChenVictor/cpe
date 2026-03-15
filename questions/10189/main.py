aafdb144982aec275762746e88d4306664ea0c7a
Victor Chen <victor.yccchen1@gmail.com>
2026-03-15 23:36:36 +0800

    study 10189

--- questions/10189/main.py
+++ questions/10189/main.py
@@ -1,48 +1,33 @@
-import sys
-
-lines = sys.stdin.read().splitlines()
-
-collection = {}
 i = 0
-j = 0
-while i < len(lines):
-    [num_of_rows, num_of_cols] = lines[i].split()
-    num_of_rows = int(num_of_rows)
-    num_of_cols = int(num_of_cols)
-    collection[j] = {}
-    collection[j]['nums'] = lines[i]
-    i += 1
-    if num_of_rows == 0:
-        break
-    collection[j]['mine'] = lines[i:i+num_of_rows]
-    collection[j]['answer'] = [[0]*num_of_cols for _ in range(num_of_rows)]
-    i += num_of_rows
-    j += 1
+while True:
+    try:
+        rows, cols = map(int, input().split())
+        if (rows == 0 or cols == 0):
+            break
+        i += 1
+        print(f"Field #{i}:")
+        matrix = []
+        for row in range(rows):
+            matrix.append(input())
 
-dirs = [(-1,-1), (-1,0), (-1,1),
-        ( 0,-1),         ( 0,1),
-        ( 1,-1), ( 1,0), ( 1,1)]
-for k in collection:
-    [num_of_rows, num_of_cols] = collection[k]['nums'].split()
-    if num_of_rows == '0':
-        continue
-    mine = collection[k]['mine']
-    num_of_rows = int(num_of_rows)
-    num_of_cols = int(num_of_cols)
-    answer = collection[k]['answer']
-    for i in range(0, num_of_rows):
-        for j in range(0, num_of_cols):
-            if(mine[i][j] == '*'):
-                for di, dj in dirs:
-                    ni, nj = i + di, j + dj
-                    if 0 <= ni < num_of_rows and 0 <= nj < num_of_cols:
-                        answer[ni][nj] += 1
-    for i in range(0, num_of_rows):
-        for j in range(0, num_of_cols):
-            if(mine[i][j] == '*'):
-                answer[i][j] = '*'
-    collection[k]['answer'] = answer
-    print(f"Field #{k + 1}:")
-    for row in answer:
-        print("".join(map(str, row)))
-    print()
+        dir = [(-1, 1), (0, 1), (1, 1),
+               (-1, 0), (0, 0), (1, 0),
+               (-1, -1), (0, -1), (1, -1)]
+        answer = []
+        for row in range(rows):
+            ans_row = ""
+            for col in range(cols):
+                if matrix[row][col] == "*":
+                    ans_row += "*"
+                else:
+                    count = 0
+                    for dx, dy in dir:
+                        nx = row + dx
+                        ny = col + dy
+                        if 0 <= nx < rows and 0 <= ny < cols and matrix[nx][ny] == "*":
+                            count += 1
+                    ans_row += str(count)
+            print(ans_row)
+        print("")
+    except EOFError:
+        break
