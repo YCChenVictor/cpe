d36c23c06f2459b3192d622ee3b056bcb4de5fd3
Victor Chen <victor.yccchen1@gmail.com>
2026-03-15 15:42:39 +0800

    study 10235

--- questions/10235/main.py
+++ questions/10235/main.py
@@ -1,21 +1,23 @@
-import sys
+is_primes = [True] * 1000000  # 0 ~ 999999
+is_primes[0] = False
+is_primes[1] = False
 
-lines = sys.stdin.read().splitlines()
-
-is_primes = [True] * (1000000 + 1) # from 0 to 1000000
-for i in range(2, int(1000000**0.5) + 1):
+for i in range(2, int(1000000 ** 0.5) + 1):
     if is_primes[i]:
-        for j in range(i * i, 1000000 + 1, i):
-            is_primes[j] = False    
+        for j in range(i * i, 1000000, i):
+            is_primes[j] = False
 
-for line in lines:
-    number = int(line)
-    rebmun = int(line[::-1])
-    if not is_primes[number]:
-        print(f'{number} is not prime.')
-    else:
-        if number != rebmun and is_primes[rebmun]:
-            print(f'{number} is emirp.')
+while True:
+    try:
+        num_input = input().strip()
+        num = int(num_input)
+        rev = int(num_input[::-1])
+
+        if not is_primes[num]:
+            print(f"{num} is not prime.")
+        elif rev != num and is_primes[rev]:
+            print(f"{num} is emirp.")
         else:
-            print(f'{number} is prime.')
-        
+            print(f"{num} is prime.")
+    except EOFError:
+        break
