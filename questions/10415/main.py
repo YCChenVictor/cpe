2fdcd94385317d2c10ba79091912a021782f93d6
Victor Chen <victor.yccchen1@gmail.com>
2026-03-16 09:47:12 +0800

    study 10415

--- questions/10415/main.py
+++ questions/10415/main.py
@@ -1,51 +1,38 @@
-import sys
-
-lines = sys.stdin.read().splitlines()
-
-num_of_melodies = int(lines[0])
-
-finger_presses = [0 for x in range(0, 10)]
-c_finger_presses = [1 if 1 <= i <= 3 or 6 <= i <= 9 else 0 for i in range(0, len(finger_presses))]
-d_finger_presses = [1 if 1 <= i <= 3 or 6 <= i <= 8 else 0 for i in range(0, len(finger_presses))]
-e_finger_presses = [1 if 1 <= i <= 3 or 6 <= i <= 7 else 0 for i in range(0, len(finger_presses))]
-f_finger_presses = [1 if 1 <= i <= 3 or 6 <= i <= 6 else 0 for i in range(0, len(finger_presses))]
-g_finger_presses = [1 if 1 <= i <= 3 else 0 for i in range(0, len(finger_presses))]
-a_finger_presses = [1 if 1 <= i <= 2 else 0 for i in range(0, len(finger_presses))]
-b_finger_presses = [1 if 1 <= i <= 1 else 0 for i in range(0, len(finger_presses))]
-C_finger_presses = [1 if 2 <= i <= 2 else 0 for i in range(0, len(finger_presses))]
-D_finger_presses = [1 if 0 <= i <= 3 or 6 <= i <= 8 else 0 for i in range(0, len(finger_presses))]
-E_finger_presses = [1 if 0 <= i <= 3 or 6 <= i <= 7 else 0 for i in range(0, len(finger_presses))]
-F_finger_presses = [1 if 0 <= i <= 3 or 6 <= i <= 6 else 0 for i in range(0, len(finger_presses))]
-G_finger_presses = [1 if 0 <= i <= 3 else 0 for i in range(0, len(finger_presses))]
-A_finger_presses = [1 if 0 <= i <= 2 else 0 for i in range(0, len(finger_presses))]
-B_finger_presses = [1 if 0 <= i <= 1 else 0 for i in range(0, len(finger_presses))]
-
+# [0,0,0,0,0,0,0,0,0,0]
+# [1,2,3,4,5,6,7,8,9,10]
+# [1,1,1,1,0,0,1,1,1,0],
 mapping = {
-    'c': c_finger_presses,
-    'd': d_finger_presses,
-    'e': e_finger_presses,
-    'f': f_finger_presses,
-    'g': g_finger_presses,
-    'a': a_finger_presses,
-    'b': b_finger_presses,
-    'C': C_finger_presses,
-    'D': D_finger_presses,
-    'E': E_finger_presses,
-    'F': F_finger_presses,
-    'G': G_finger_presses,
-    'A': A_finger_presses,
-    'B': B_finger_presses
+    'c': [0,1,1,1,0,0,1,1,1,1],
+    'd': [0,1,1,1,0,0,1,1,1,0],
+    'e': [0,1,1,1,0,0,1,1,0,0],
+    'f': [0,1,1,1,0,0,1,0,0,0],
+    'g': [0,1,1,1,0,0,0,0,0,0],
+    'a': [0,1,1,0,0,0,0,0,0,0],
+    'b': [0,1,0,0,0,0,0,0,0,0],
+    'C': [0,0,1,0,0,0,0,0,0,0],
+    'D': [1,1,1,1,0,0,1,1,1,0],
+    'E': [1,1,1,1,0,0,1,1,0,0],
+    'F': [1,1,1,1,0,0,1,0,0,0],
+    'G': [1,1,1,1,0,0,0,0,0,0],
+    'A': [1,1,1,0,0,0,0,0,0,0],
+    'B': [1,1,0,0,0,0,0,0,0,0],
 }
 
-for i in range(1, num_of_melodies + 1):
-    presses = [0 for x in range(0, 10)]
-    current = [0 for x in range(0, 10)]
-    notes = list(lines[i])
-    for j in range(0, len(notes)):
-        next_move = mapping[notes[j]]
-        for k in range(0, len(next_move)):
-            if(next_move[k] == 1 and current[k] == 0):
-                presses[k] += 1
-        current = next_move
 
-    print(" ".join(map(str, presses)))
+melodies = int(input())
+for m in range(melodies):
+    melody = input()
+    # print(melody)
+    if melody == "":
+        print("0 0 0 0 0 0 0 0 0 0")
+        continue
+
+    press = [0,0,0,0,0,0,0,0,0,0]
+    current = [0,0,0,0,0,0,0,0,0,0]
+    for note in melody:
+        next = mapping[note]
+        for i in range(len(press)):
+            if current[i] == 0 and next[i] == 1:
+                press[i] += 1
+        current = next
+    print(" ".join(map(str, press)))
